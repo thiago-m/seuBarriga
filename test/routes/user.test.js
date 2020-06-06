99362e2b1daf2fe13105d1fe35c6b4189387b24d
Thiago Marciel F Silva <th1ago_m@hotmail.com>
2020-06-06 18:34:15 -0300

    teste para saber se existe validação em usuarios sem email

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -28,3 +28,11 @@ test('Não deve inserir usuário sem nome', () => {
 			expect(res.body.error).toBe('Nome é um atributo obrigatório')
 		})
 })
+
+test('Não deve inserir usuário sem email', async () => {
+	const result = await request(app).post('/users')
+		.send({name: 'user teste', password: '123'})
+	
+	expect(result.status).toBe(400)
+	expect(result.body.error).toBe('Email é um atributo obrigatório')
+})
