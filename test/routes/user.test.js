1da880f95eefebdca5fa785b0d4e118b4d7bc4fe
Thiago Marciel F Silva <th1ago_m@hotmail.com>
2020-06-06 18:40:35 -0300

    teste para validar se usuario esta inserindo a senha

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -36,3 +36,12 @@ test('Não deve inserir usuário sem email', async () => {
 	expect(result.status).toBe(400)
 	expect(result.body.error).toBe('Email é um atributo obrigatório')
 })
+
+test('Não deve inserir usuário sem senha', async done => {
+	const result = await request(app).post('/users')
+		.send({name: 'user teste', mail: 'teste@mail.com'})
+	
+	expect(result.status).toBe(400)
+	expect(result.body.error).toBe('Senha é um atributo obrigatório')
+	done()
+})
