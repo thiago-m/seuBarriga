01b80cc7f4907d77340008c6a6faac8f5a2b08ee
Thiago Marciel F Silva <th1ago_m@hotmail.com>
2020-06-13 16:37:21 -0300

    adicionado serviço para buscar contas

--- src/services/account.js
+++ src/services/account.js
@@ -2,6 +2,10 @@ module.exports = app => {
     const save = account => {
         return app.db('accounts').insert(account, '*')
     }
+    
+    const findAll = () => {
+        return app.db('accounts')
+    }
 
-    return { save }
+    return { save, findAll }
 }
